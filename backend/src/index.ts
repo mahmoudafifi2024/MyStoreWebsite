2b7cdd5f358d1a61410572dbf493cd7721c076ef
Mahmoud Afifi <mahmoudafifi2024@gmail.com>
2025-03-04 00:26:12 +0200

    create .env for BackEnd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,30 @@ import express from "express";
 import mongoose from "mongoose";
 import productsRouter from "./routers/products";
 import cors from "cors";
+import dotenv from "dotenv";
 
-
+dotenv.config(); 
 
 const app = express();
-const port =  5002;
-const mongoUri =  "mongodb://localhost:27017/my-store";
-const frontendUrl =  "http://localhost:5173";
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+const frontendUrl = process.env.CLIENT_URL;
+
+console.log(mongoUri)
+console.log(frontendUrl)
+
+if (!mongoUri || !frontendUrl) {
+    console.error("Missing required environment variables: MONGO_URI or CLIENT_URL");
+    process.exit(1);
+}
 
 app.use(cors({ origin: frontendUrl }));
 app.use(express.json());
 
 mongoose
-    .connect(mongoUri).then(() => console.log("Connected to MongoDB"));
+    .connect(mongoUri)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/", productsRouter);
 
